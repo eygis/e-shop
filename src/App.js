cf558ea0057a2446841b707fbfe438ba9d30a99f
eygis <86146074+eygis@users.noreply.github.com>
2021-10-13 17:40:21 +0900

    2021/10/13

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ class App extends React.Component {
     super(props);
     this.state = {}
   }
-  
+  hamburgerFunction = () => {
+    document.getElementById("hamburgerMenuArea").style.display="block"
+  }
+
+
   render() {
 
+    window.onclick = (event) => {
+      if (event.target === document.getElementById("hamburgerMenuArea")) {
+        document.getElementById("hamburgerMenuArea").style.display="none"
+      }
+    }
+
   return (
-    <div className="App">
-      filler text
+    <div className="wrapper">
+      <div id="header">
+      <div id="hamburgerDiv">
+        <i id="hamburger" className="fa fa-bars" onClick={this.hamburgerFunction}></i>
+        </div>
+      <div id="hamburgerMenuArea">
+        <div id="hamburgerMenu">
+          this is the menu
+        </div>
+      </div>
+      <h1 id="title">Beans & Leaves</h1>
+      <span id="subTitle">Your source for local coffee and tea!</span>
+      </div>
     </div>
   );
   }
