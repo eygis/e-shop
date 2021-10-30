bb54aa89eba5ea107166877f2cba810a83cf8eeb
eygis <86146074+eygis@users.noreply.github.com>
2021-10-30 19:00:40 +0900

    21/10/30 tried adding images to the product display, looked bad. Leaving the sources in the products array just in case

--- src/products.js
+++ src/products.js
@@ -3,43 +3,50 @@ let products = [
         "type": "coffee",
         "name": "ethiopia",
         "blend": "medium",
-        "taste": ["chocolate, ", "medium body, ", "high acidity"]
+        "taste": ["chocolate, ", "medium body, ", "high acidity"],
+        "picture": "https://cdn.pixabay.com/photo/2017/09/29/02/12/coffee-2797638_960_720.jpg"
     },
     {
         "type": "coffee",
         "name": "kenya",
         "blend": "medium",
-        "taste": ["citrus, ", "medium body, ", "medium acidity"]
+        "taste": ["citrus, ", "medium body, ", "medium acidity"],
+        "picture": "https://images.unsplash.com/photo-1489392191049-fc10c97e64b6?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1467&q=80"
     },
     {
         "type": "coffee",
         "name": "blue mountain",
         "blend": "medium",
-        "taste": ["creamy"]
+        "taste": ["creamy"],
+        "picture": "https://images.unsplash.com/photo-1557129603-ff35a0c70e5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
     },
     {
         "type": "coffee",
         "name": "french",
         "blend": "dark",
-        "taste": ["smoky"]
+        "taste": ["smoky"],
+        "picture": "https://images.unsplash.com/photo-1586253181808-c030e7e1aa5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80"
     },
     {
         "type": "tea",
         "name": "earl grey", 
         "blend": "black", 
-        "taste": ["fruity"]
+        "taste": ["fruity"],
+        "picture": "https://cdn.pixabay.com/photo/2014/08/16/13/38/tee-419320_960_720.jpg"
     },
     {
         "type": "tea",
         "name": "barley tea", 
         "blend": "other",
-        "taste": ["barley"]
+        "taste": ["barley"],
+        "picture": "https://images.unsplash.com/photo-1437252611977-07f74518abd7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80"
     },
     {
         "type": "tea",
         "name": "matcha",
         "blend": "green",
-        "taste": ["bitter"]
+        "taste": ["bitter"],
+        "picture": "https://images.unsplash.com/photo-1566373049939-704ea187ef98?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=871&q=80"
     },
 ]
     
